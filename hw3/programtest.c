99582fad9d3511a3e6dc14f7f9c3e3d4a403be13
mehmetcakmak <tr.mehmetcakmak@gmail.com>
2018-11-24 04:35:48 +0300

    mcakmak, hw4 cogu bitti

--- hw3/programtest.c
+++ hw3/programtest.c
@@ -1,12 +1,25 @@
 #include<stdio.h>
-void myPrintArr(int arr[],boyut);
+#include<stdlib.h>
+#include<time.h>
+
+#define ARR_X  3
+#define ARR_Y  3
+#define ARR_SIZE 9
+
+int validation(int arr[]);
+void myPrintArr(int arr[]);
 
 int main(){
-	int i,j,row=0,column=0;
-	int dizi[]={9,4,5,8,6,7,1,3,0,2};
-	
+	srand((unsigned)time(NULL));
+	int dizi[]={4,5,8,6,7,1,3,0,2};
+	printf("size_t sum : %d, item  : %d \n",(int)sizeof(dizi),(int)sizeof(dizi[0]));
 	myPrintArr(dizi);
-	
+	for(int index = 0;index<100;index++){
+		int aa1= rand()%2;
+		int aa2 = rand()%3;
+		int aa3 = rand()%4;
+		printf("aa1 : %d, aa2 : %d , aa3 : %d  \n",aa1,aa2,aa3);
+	}
 	/*for(i=0;i<10;i++){
 		if(i<=2){
 			printf("%d ",dizi[i]);
@@ -18,19 +31,120 @@ int main(){
 			printf("%d ",dizi[i]);
 		}
 	column++;
-	}
-	for(j=0;j<10;j++){
-		if(dizi[j]==0){
-			printf("Dizinin %d. satır %d kolonunun %d. Değeri Sıfırdır.\n",row,column,j);
-		}
 	}*/
+	
+	int space_index=-1;
+	
+	for(int j=0;j<10;j++){
+		if(dizi[j]==0){
+			space_index = j;
+		}
+	}
+	
+	int changedNumber = 0;
+	
+	while(validation(dizi)<0){
+		changedNumber++;
+		printf("ccc : %d\n",changedNumber);
+		myPrintArr(dizi);
+		int row = space_index/ARR_X;
+		int column = space_index%ARR_X;
+		if(row==0 && column==0){   //LEFT TOP
+			int tmp[2] = { row*ARR_X+column+1, (row+1)*ARR_X+column};
+			int my_random = rand()%2;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if(row==0 && column==(ARR_X-1)){  // RIGHT TOP
+			int tmp[2] = { row*ARR_X+column-1, (row+1)*ARR_X+column};
+			int my_random = rand()%2;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if(row==(ARR_Y-1) && column==0){  // LEFT BOTTOM
+			int tmp[2] = { row*ARR_X+column+1, (row-1)*ARR_X+column};
+			int my_random = rand()%2;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if(row==(ARR_Y-1) && column==0){  // RIGHT BOTTOM
+			int tmp[2] = { row*ARR_X+column-1, (row-1)*ARR_X+column};
+			int my_random = rand()%2;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if(row==0 && column!=0){   //MIDDLE TOP
+			int tmp[3] = { row*ARR_X+column+1,row*ARR_X+column-1, (row+1)*ARR_X+column};
+			int my_random = rand()%3;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if(row==0 && column==(ARR_X-1)){  // RIGHT MIDDLE
+			int tmp[3] = { row*ARR_X+column-1, (row+1)*ARR_X+column,(row-1)*ARR_X+column};
+			int my_random = rand()%3;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if(row==(ARR_Y-1) && column==0){  // LEFT MIDDLE
+			int tmp[3] = { row*ARR_X+column+1, (row+1)*ARR_X+column,(row-1)*ARR_X+column};
+			int my_random = rand()%3;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if(row==(ARR_Y-1) && column==0){  // MIDDLE BOTTOM
+			int tmp[3] = { row*ARR_X+column+1,row*ARR_X+column-1, (row-1)*ARR_X+column};
+			int my_random = rand()%3;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		if((row!=0 || row!=(ARR_Y-1)) && (column != (ARR_X-1) || column!=0)){  // MIDDLE BOTTOM
+			int tmp[4] = { row*ARR_X+column+1,row*ARR_X+column-1, (row-1)*ARR_X+column,(row+1)*ARR_X+column};
+			int my_random = rand()%4;
+			dizi[space_index] = dizi[tmp[my_random]];
+			dizi[tmp[my_random]] = 0;
+			space_index = tmp[my_random];
+		}
+		
+		
+	}
+	printf("Sonuc hesaplandı : %d\n",changedNumber);
+	myPrintArr(dizi);
+	printf("\n\n\n");
+	
+	return 0;
 }
 
+int validation(int arr[]){
+	for(int i=0;i<ARR_SIZE;i++){
+		if(arr[i]>arr[i+1])
+			return -1;
+			
+	}
+	
+	return 1;
+}
+			
 
-void myPrintArr(int arr[],boyut){
-	for(int i=0;i<arr.length;i++){
+void myPrintArr(int arr[]){
+	for( int i=0; i<ARR_SIZE; i++ ){
 		printf("%d",arr[i]);
-		if((i+1)%boyut==0)
+		if((i+1)%ARR_X==0)
 			printf("\n");
 		else
 			printf(",");
