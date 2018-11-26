8aeb0b1bc2b725a5be926b6a95a3fce98b872c80
mehmetcakmak <tr.mehmetcakmak@gmail.com>
2018-11-27 00:22:10 +0300

    mcakmak,hw4 projesinin en son hali ama hata vermediği halde çalışmıyor

--- mytest/program.c
+++ mytest/program.c
@@ -1,140 +1,135 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-//int my_print(int arr[][3]);
-void findSpace(int const arr[][3],int spaceIndex[]);
-int testFindSpace(int spaceArr[]);
-//void change_space(int arr[][3]);
-//int siralama(int arr[][3]);
-
 #define ARR_X 3
 #define ARR_Y 3
-//#define row_x 3
-//#define column_y 3
+
+void findSpace(int const arr[][3],int spaceIndex[]);
+int validSort(int arr[][ARR_Y]);
+void changeSpace(int arr[][ARR_Y],int spaceIndex[]);
+int my_print(int arr[][3]);
+
 
 int main(){
 		//[satır][sütun]
 	int dizi[ARR_X][ARR_Y]={{8,0,7},{4,2,6},{5,1,3}};
 	
 	int spaceIndex[2] = {-1,-1}; //initialize
-	findSpace(dizi,spaceIndex);
-	
-	if(!testFindSpace(spaceIndex)){
-		printf("SpaceIndex değeri bulunamadı. program sonlandırıldı.\n");
-		return 0;
-	}
-	
-	printf("SpaceIndex successfull\n");
-	
-	while(!validSort(dizi)){
-		changeSpace(dizi,spaceIndex);			
-	}
-	//change_space(dizi);
-	//my_print(dizi);
-	
-	
+		
+	while(!(validSort(dizi))){
+	printf("mehmet\n");
+		changeSpace(dizi,spaceIndex);
+		my_print(dizi);
+		
+		}
+		
 	return 0;
 }
 
-
 void changeSpace(int arr[][ARR_Y],int spaceIndex[]){
+	
 
 	int row=spaceIndex[0];
 	int column=spaceIndex[1];
 	
-		if(spaceIndex[0]==0 && spaceIndex[1]==0) //RULE ONE : LEFT TOP CORNER	
+		if(row==0 && column==0) //RULE ONE : LEFT TOP CORNER	
 		{
 			//to do.
-			int tmp[2] = { row*ARR_X+column+1, (row+1)*ARR_X+column };
-				int my_random = rand()%2;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random]; 
+			int tmp[2][2] = { {row,column+1}, {row+1,column} }; //tmp[0][0] = row, tmp[0][1] = column+1, tmp[1][0] = row+1
+				int myRandom = rand()%2;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
 		}
 		
-		if(spaceIndex[0]==0 && spaceIndex[1]==2) //RULE TWO : RİGHT TOP CORNER
+		if(row==0 && column==2) //RULE TWO : RİGHT TOP CORNER
 		{
 			//to do.
-			int tmp[2] = { row*ARR_X+column-1, (row-1)*ARR_X+column };
-				int my_random = rand()%2;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random];
+			int tmp[2][2] = { {row,column-1}, {row+1,column-2} }; //tmp[0][0] = row, tmp[0][1] = column+1, tmp[1][0] = row+1
+				int myRandom = rand()%2;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
 		}
 		
-		if(spaceIndex[0]==2 && spaceIndex[1]==0) //RULE THREE : LEFT BOTTOM CORNER
+		if(row==2 && column==0) // RULE THREE : LEFT BOTTOM CORNER
 		{
-			//to do.
-			int tmp[2] = { row*ARR_X+column+1, (row+1)*ARR_X+column };
-				int my_random = rand()%2;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random];
+			int tmp[2][2] = { {row,column+1}, {row-1,column} }; //tmp[0][0] = row, tmp[0][1] = column+1, tmp[1][0] = row+1
+				int myRandom = rand()%2;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
 		}
 		
-		if(spaceIndex[0]==2 && spaceIndex[1]==2) //RULE FOUR : RİGHT TOP CORNER
+		if(row==2 && column==2) // RULE FOUR : RİGHT BOTTOM CORNER
 		{
 			//to do.
-			int tmp[2] = { row*ARR_X+column-1, (row-1)*ARR_X+column };
-				int my_random = rand()%2;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random];
-		}
-		
-		
-		if(spaceIndex[0]==0 && !(spaceIndex[1]==0 || spaceIndex[1]==2)) //RULE FİVE : MİDLE TOP	
-		{
-			//to do.
-			int tmp[3] = { row*ARR_X+column+1,row*ARR_X+column-1, (row+1)*ARR_X+column };
-				int my_random = rand()%3;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random]; 
-		}
-		
-		if(!(spaceIndex[0]==0 || spaceIndex[0]==2) && spaceIndex[1]==0) //RULE SİX : LEFT MİDLE
-		{
-			//to do.
-			int tmp[3] = { row*ARR_X+column+1, (row-1)*ARR_X+column , (row+1)*ARR_X+column };
-				int my_random = rand()%3;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random];
-		}
-		
-		if(spaceIndex[0]==1 && !(spaceIndex[1]==0 || spaceIndex[1]==1)) //RULE SEVEN : RİGHT MİDLE
-		{
-			//to do.
-			int tmp[3] = { row*ARR_X+column-1, (row+1)*ARR_X+column , (row-1)*ARR_X+column };
-				int my_random = rand()%3;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random];
-		}
-		
-		if(spaceIndex[0]==2 && !(spaceIndex[1]==0 || spaceIndex[1]==2)) //RULE EİGHT : BOTTOM MİDLE
-		{
-			//to do.
-			int tmp[3] = { row*ARR_X+column-1, row*ARR_X+column+1, (row-1)*ARR_X+column };
-				int my_random = rand()%3;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random];
+			int tmp[2][2] = { {row,column-1}, {row-1,column} }; //tmp[0][0] = row, tmp[0][1] = column+1, tmp[1][0] = row+1
+				int myRandom = rand()%2;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
 		}
 		
 		
-		if(!(spaceIndex[0]==0 || spaceIndex[0]==2)  && !(spaceIndex[1]==0 || spaceIndex[1]==2)) //RULE NİNE : MİDLE
+		if(row==0 && !(column==0 || column==2)) // RULE FİVE : MİDLE TOP	
 		{
 			//to do.
-			int tmp[4] = { row*ARR_X+column-1, row*ARR_X+column+1, (row-1)*ARR_X+column ,(row+1)*ARR_X+column };
-				int my_random = rand()%4;
-				arr[spaceIndex] = arr[tmp[my_random]];
-				arr[tmp[my_random]] = 0;
-				spaceIndex = tmp[my_random];
+			int tmp[3][2] = { {row,column-1}, {row,column+1}, {row-1,column} }; 
+				int myRandom = rand()%3;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
 		}
 		
-	
+		if(!(row==0 || row==2) && column==0) //RULE SİX : LEFT MİDLE
+		{
+			//to do.
+			int tmp[3][2] = { {row,column+1}, {row-1,column}, {row-1,column} }; 
+				int myRandom = rand()%3;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
+		}
+		
+		if(row==1 && !(column==0 || column==1)) //RULE SEVEN : RİGHT MİDLE
+		{
+			//to do.
+			int tmp[3][2] = { {row,column-1}, {row+1,column}, {row-1,column} }; 
+				int myRandom = rand()%3;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
+		}
+		
+		if(row==2 && !(column==0 || column==2)) //RULE EİGHT : BOTTOM MİDLE
+		{
+			//to do.
+			int tmp[3][2] = { {row,column-1}, {row,column+1}, {row-1,column} }; 
+				int myRandom = rand()%3;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
+		}
+		
+		
+		if(!(row==0 || row==2)  && !(column==0 || column==2)) //RULE NİNE : MİDLE
+		{
+			//to do.
+			int tmp[4][2] = { {row,column-1}, {row,column+1}, {row-1,column}, {row+1,column} }; 
+				int myRandom = rand()%4;
+				arr[row][column] = arr[tmp[myRandom][0]][tmp[myRandom][1]];
+				arr[tmp[myRandom][0]][tmp[myRandom][1]] = 0;
+				row =  tmp[myRandom][0];
+				column = tmp[myRandom][1];
+		}
 }
 
 
@@ -161,48 +156,11 @@ void findSpace(int const arr[][ARR_Y],int spaceIndex[]){
 			}	
 }
 
-int testFindSpace(int spaceArr[]){
-	if(spaceArr[0]==0 && spaceArr[1]==1)
-		return 1;
-	return -1;
-}
-/*
 int my_print(int arr[][3]){
-		
 	for(int i=0;i<3;i++){
 		for(int j=0;j<3;j++){
 		printf("%d ",arr[i][j]);
 		}
 		printf("\n");
 	}
-	
 }
-
-void change_space(int arr[][3],int spaceIndex){
-	while(siralama(arr)<0){
-		//sol üst köşe
-		
-		//sağ üst köşe
-		//sol alt köşe
-		//sağ alt köşe
-		
-		//üst orta
-		//alt orta
-		//sol orta
-		//sağ orta
-		
-		//en orta
-	}
-}
-
-int siralama(int arr[][3]){
-	for(int i=0;i<3;i++){
-		for(int j=0;j<3;j++)
-		if(arr[i][j]>arr[i+1][j+1])
-			return -1;
-			
-	}
-	
-	return 1;
-}
-*/
