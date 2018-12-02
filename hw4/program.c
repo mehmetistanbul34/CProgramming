9bc0c6eb9e64c51ac6a51390b65a9a54713754f8
mehmetcakmak <tr.mehmetcakmak@gmail.com>
2018-12-03 01:15:29 +0300

    mcakmak,kural 3 başarılı bir şekilde tamamlanmıştır

--- hw4/program.c
+++ hw4/program.c
@@ -66,7 +66,15 @@ void chandeSpace(int arr[9],int spaceIndex){	// Space Change Function
 		arr[tmp[random]]=0;
 		spaceIndex=tmp[random];	
 	}
-
+	
+	if(row==ARR_Y-1 && column==0){	// Left Bottom Corner
+		int tmp[2]={row*ARR_X+column+1,(row-1)*ARR_X+column};	//bir yana yada bir alta kaydırılmasını sağlar
+		int random = rand()%2;	// Random olarak 1 veya 2 sayılarından birini seçer 
+		arr[spaceIndex]=arr[tmp[random]];
+		arr[tmp[random]]=0;
+		spaceIndex=tmp[random];	
+	}
+	
 }
 
 void myPrint(int arr[9]){	// Diziyi ekrana bastıran fonksiyon
