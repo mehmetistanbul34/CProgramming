32c0b3fbd2184a1b07a0c2ad8cbe28ea4570a5f1
mehmetcakmak <tr.mehmetcakmak@gmail.com>
2018-12-02 17:15:04 +0300

    mcakmak,bu defa da sıfırla yerini değiştirmek istediğim sayıyının üstüne sıfır yazıyor yine olmadı

--- ODEV/program.c
+++ ODEV/program.c
@@ -12,8 +12,12 @@ void myPrint(int arr[9]);
 
 
 int main(){
+
+	
+	srand((unsigned)time(NULL));
+
 	int k,spaceIndex=0;
-	int dizi[9]={8,7,6,5,4,3,2,1,0};
+	int dizi[9]={8,0,6,5,4,3,2,1,7 };
 	
 	for(k=0;k<ARR_SIZE;k++){
 		if(dizi[k]==0){
@@ -32,39 +36,37 @@ int main(){
 
 void changeSpace(int arr[9],int spaceIndex){
 
-	srand((unsigned)time(NULL));
-
 	int row=spaceIndex/ARR_X;
 	int column=spaceIndex%ARR_Y;
 	
 	printf("SpaceIndex  :%d\tRow : %d\tColumn : %d\n",spaceIndex,row,column);
 	if(row==0 && column==0){	// Left Top Corner
-		int tmp[2] = { row+1,column+1};
-			int random = rand()%2;
+		int tmp[2] = {  row*ARR_X+column+1, (row+1)*ARR_X+column};
+			int random = rand()%2;	//random olarak 0 yada 1 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 	
-	if(row==0 && column==2){	// Right Top Corner
-		int tmp[2] = { row+1,column-1};
-			int random = rand()%2;
+	if(row==0 && column==(ARR_X-1)){	// Right Top Corner
+		int tmp[2] = {  row*ARR_X+column-1, (row+1)*ARR_X+column};
+			int random = rand()%2;	//random olarak 0 yada 1 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 	
-	if(row==2 && column==0){	// Left Bottom Corner
-		int tmp[2] = { row-1,column+1};
-			int random = rand()%2;
+	if(row==(ARR_Y-1) && column==0){	// Left Bottom Corner
+		int tmp[2] = {  row*ARR_X+column-1, (row+1)*ARR_X+column};
+			int random = rand()%2;	//random olarak 0 yada 1 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 	
-	if(row==2 && column==2){	// Right Bottom Corner
-		int tmp[2] = { row-1,column-1};
-			int random = rand()%2;
+	if(row==(ARR_Y-1) && column==(ARR_X-1)){	// Right Bottom Corner
+		int tmp[2] = { row*ARR_X+column-1, (row-1)*ARR_X+column};
+			int random = rand()%2;	//random olarak 0 yada 1 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
@@ -72,60 +74,60 @@ void changeSpace(int arr[9],int spaceIndex){
 	
 	
 	if(row==0 && column==1){	// Top Midle
-		int tmp[3] = { row+1,column-1,column+1};
-			int random = rand()%3;
+		int tmp[3] = { row*ARR_X+column+1,(row-1)*ARR_X+column, (row+1)*ARR_X+column};
+			int random = rand()%3;	//random olarak 0 , 1 veya 2 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 	
 	if(row==1 && column==0){	// Left Midle
-		int tmp[3] = { row+1,row-1,column+1};
-			int random = rand()%3;
+		int tmp[3] = {  row*ARR_X+column+1,row*ARR_X+column-1, (row+1)*ARR_X+column};
+			int random = rand()%3;	//random olarak 0 , 1 veya 2 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 	
-	if(row==1 && column==2){	// Right Midle
-		int tmp[3] = { row+1,row-1,column-1};
-			int random = rand()%3;
+	if(row==1 && column==(ARR_X-1)){	// Right Midle
+		int tmp[3] = {  row*ARR_X+column-1,row*ARR_X+column+1, (row-1)*ARR_X+column};
+			int random = rand()%3;	//random olarak 0 , 1 veya 2 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 	
-	if(row==2 && column==1){	// Bottom Midle
-		int tmp[3] = { row-1,column-1,column+1};
-			int random = rand()%3;
+	if(row==(ARR_Y-1) && column==1){	// Bottom Midle
+		int tmp[3] = {  row*ARR_X+column-1,(row-1)*ARR_X+column, (row+1)*ARR_X+column};
+			int random = rand()%3;	//random olarak 0 , 1 veya 2 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 	
 	if(row==1 && column==1){	// Midle
-		int tmp[4] = { row+1,row-1,column-1,column+1};
-			int random = rand()%4;
+		int tmp[4] = {  row*ARR_X+column-1,row*ARR_X+column+1,(row-1)*ARR_X+column, (row+1)*ARR_X+column};
+			int random = rand()%4;	//random olarak 0 , 1 , 2 veya 3 sayılarından birini seçer
 			arr[spaceIndex] = arr[tmp[random]];
 			arr[tmp[random]] = 0;
 			spaceIndex = tmp[random];
 	}
 }
 
-int siralimi(int arr[9]){
+int siralimi(int arr[9]){	//Bu fonksiyon dizinin sıralı olup olmadığını kontrol eder
 	int a;
 	for(a=0;a<ARR_SIZE-1;a++){
 		if(arr[a]>arr[a+1])
-		return -1;
+		return -1;	// dizinin sıralı değil ise -1 return eder
 	}
 	
-	return 1;
+	return 1;	// dizinin sıralı ise 1 return eder
 }
 
-void myPrint(int arr[9]){
+void myPrint(int arr[9]){	// Bu fonksiyon diziyi ekrana bastırır
 	int i;
 	for(i=0;i<ARR_SIZE;i++){
-		if(i%3!=0 || i==0){
+		if(i%3!=0 || i==0){	// Bu satır diziyi 3*3 şeklinde ekrana basar
 			printf("%d ",arr[i]);
 		}
 		else{
